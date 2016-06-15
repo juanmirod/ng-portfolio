34a431b8df0071f46afaddd03faa84cfdefaca4b
Juanmi Rodriguez Cerón <juanmi.rodriguez.ceron@gmail.com>
2016-06-15 16:41:01 +0200

    Menu toggle in place, added ng-animate to dependencies

--- app/app.js
+++ app/app.js
@@ -3,12 +3,19 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngAnimate',
+  'ui.bootstrap.collapse',
   'myApp.about',
   'myApp.view2',
   'myApp.version'
 ]).
+
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/about'});
+}]).
+
+controller('AppController', ['$scope', function($scope){
+  $scope.isCollapsed = true;
 }]);
